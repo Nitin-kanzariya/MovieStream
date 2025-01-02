01f6d7feda53fd5b0bd37523e85811ca4e638cf1
Nitin Kanzariya <136853621+Nitin-kanzariya@users.noreply.github.com>
2025-01-02 22:11:40 +0530

    change configFile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api/": "http://localhost:3000",
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/": "http://localhost:3000",
-    },
+  build: {
+    outDir: "dist", // Ensure the output directory is "dist"
+    chunkSizeWarningLimit: 2000, // Adjust chunk size limit for large bundles
   },
+  server: {},
 });
-
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:3000', // Backend URL
-//         changeOrigin: true, // Ensures proper handling of host headers
-//         secure: false, // Allows self-signed certificates (optional)
-//         rewrite: (path) => path.replace(/^\/api/, '') // Remove the /api prefix before forwarding
-//       }
-//     }
-//   }
-// });
