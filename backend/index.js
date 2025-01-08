f40b98d67aec0ae1fd0b9d3b8ee5d6af120a001f
Nitin-Kanzariya <nitinkanzariya112233@gmail.com>
2025-01-08 10:13:30 +0530

    MovieStream commited

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,11 @@ const app = express();
 // Allow requests from any origin
 app.use(
   cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: [
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
